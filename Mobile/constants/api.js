1c7d724c9974b01bbab7e48ba52e380726104d28
Sufyan Liaqat <sufyanliaquat58@gmail.com>
2025-08-03 00:25:34 +0500

    update API_URL to use production endpoint

--- Mobile/constants/api.js
+++ Mobile/constants/api.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://10.0.2.2:7000/api"
+export const API_URL = "https://movie-app-backend-2icx.onrender.com/api"
 
 export const sleep = (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
